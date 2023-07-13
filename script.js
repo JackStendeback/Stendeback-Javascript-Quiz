9187afbb0becf819954b214ef7716cb81de5bb2f
Jack Stendeback <jackstendeback@jacks-mbp.lan>
2023-07-13 01:58:49 -0500

    adding framework for the quizdata, eventually putting actual questions in for placeholders

---
+++ script.js
@@ -0,0 +1,62 @@
+// Quiz Data
+const quizData = [
+    {
+        question: "Question 1",
+        options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+        answerIndex: 0 // Index of the correct answer
+    },
+
+    {
+        question: "Question 2",
+        options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+        answerIndex: 0 // Index of the correct answer
+    },
+
+    {
+        question: "Question 3",
+        options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+        answerIndex: 0 // Index of the correct answer
+    },
+
+    {
+        question: "Question 4",
+        options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+        answerIndex: 0 // Index of the correct answer
+    },
+
+    {
+        question: "Question 5",
+        options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+        answerIndex: 0 // Index of the correct answer
+    },
+
+    {
+        question: "Question 6",
+        options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+        answerIndex: 0 // Index of the correct answer
+    },
+
+    {
+        question: "Question 7",
+        options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+        answerIndex: 0 // Index of the correct answer
+    },
+
+    {
+        question: "Question 8",
+        options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+        answerIndex: 0 // Index of the correct answer
+    },
+
+    {
+        question: "Question 9",
+        options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+        answerIndex: 0 // Index of the correct answer
+    },
+
+    {
+        question: "Question 10",
+        options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+        answerIndex: 0 // Index of the correct answer
+    },
+];
